045b9936b0b91d07a6872be43ac987227f7140c3
Asel03 <tulebaevaasel03@gmail.com>
2023-05-26 18:08:39 +0600

    something

--- server.go
+++ server.go
@@ -5,22 +5,7 @@ import (
 	"net/http"
 )
 
-type httpHandler struct {
-	message string
-}
-
-func (h httpHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(resp, h.message)
-}
-
 func main() {
-
-	h1 := httpHandler{message: "Index"}
-	h2 := httpHandler{message: "About"}
-
-	http.Handle("/", h1)
-	http.Handle("/about", h2)
-
 	fmt.Println("Server is listening...")
-	http.ListenAndServe(":8181", nil)
+	http.ListenAndServe(":8181", http.FileServer(http.Dir("static")))
 }
