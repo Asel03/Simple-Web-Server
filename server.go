ff4d5f3665376e75d8cb1e84d58a794f06332a96
Asel03 <tulebaevaasel03@gmail.com>
2023-05-27 12:10:43 +0600

    something

--- server.go
+++ server.go
@@ -6,6 +6,16 @@ import (
 )
 
 func main() {
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "About Page")
+	})
+	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Contact Page")
+	})
 	fmt.Println("Server is listening...")
-	http.ListenAndServe(":8181", http.FileServer(http.Dir("static")))
+	http.ListenAndServe(":8181", nil)
 }
