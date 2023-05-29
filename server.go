c6c80cadf0fee6f78b09628d8dc1903cfb7bf178
Asel03 <tulebaevaasel03@gmail.com>
2023-05-29 10:14:51 +0600

    something

--- server.go
+++ server.go
@@ -10,15 +10,19 @@ import (
 func productsHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-	cat := vars["category"]
-	response := fmt.Sprintf("Product category=%s id=%s", cat, id)
+	response := fmt.Sprintf("id=%s", id)
 	fmt.Fprint(w, response)
+}
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Index Page")
 }
 
 func main() {
 
 	router := mux.NewRouter()
-	router.HandleFunc("/products/{category}/{id:[0-9]+}", productsHandler)
+	router.HandleFunc("/products/{id:[0-9]+}", productsHandler)
+	router.HandleFunc("/articles/{id:[0-9]+}", productsHandler)
+	router.HandleFunc("/", indexHandler)
 	http.Handle("/", router)
 
 	fmt.Println("Server is listening...")
