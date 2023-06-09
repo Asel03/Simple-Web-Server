663110f0adb4527dafa2f62086a6538a44f05f59
Asel03 <tulebaevaasel03@gmail.com>
2023-06-09 11:21:35 +0600

    something

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	// Обработчик для CSS файлов
+	http.HandleFunc("/styles.css", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "styles.css")
+	})
+
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8282", nil)
+}
